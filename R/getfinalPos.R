7e1079cdea650a399cf0b24aea8658be135d224b
david-salgado <david.salgado.fernandez@gmail.com>
2018-11-11 22:59:41 +0100

    Create getfinalPos

---
+++ R/getfinalPos.R
@@ -0,0 +1,36 @@
+#' @title Return variable final positions from the input schema.
+#'
+#' @description \code{getfinalPos} extracts the slot \code{df} of the input object and returns its
+#' column \code{finalPos} as a vector.
+#'
+#' @param object Object of class \linkS4class{StfwfSchema}.
+#'
+#' @return An integer vector.
+#'
+#' @examples
+#' # A trivial example:
+#' df <- data.frame(variable = c('Turnover', 'Employees'),
+#'                  length = c(9L, 3L),
+#'                  initialPos = c(1, 10),
+#'                  finalPos = c(9, 12),
+#'                  type = rep('num', 2),
+#'                  valueRegEx = c('[0-9]{0,9}', '[0-9]{0,3}'),
+#'                  description = c('Turnover of the business unit',
+#'                                  'Number of employees of the business unit'),
+#'                  stringsAsFactors = FALSE)
+#' Schema <- new(Class = 'StfwfSchema', df = df)
+#' getfinalPos(Schema)
+#'
+#'
+#' @export
+setGeneric("getfinalPos", function(object){standardGeneric("getfinalPos")})
+
+#' @rdname getfinalPos
+#'
+#' @export
+setMethod(
+  f = "getfinalPos",
+  signature = c("StfwfSchema"),
+  function(object){object@df[['finalPos']]}
+)
+
