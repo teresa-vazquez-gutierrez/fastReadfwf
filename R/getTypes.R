ab38135794bc95395def114d0e19972f8edaa8c6
david-salgado <david.salgado.fernandez@gmail.com>
2018-11-11 23:03:20 +0100

    Create getTypes

---
+++ R/getTypes.R
@@ -0,0 +1,37 @@
+#' @title Return variable types from the input schema.
+#'
+#' @description \code{getTypes} extracts the slot \code{df} of the input object and returns its
+#' column \code{type} as a vector.
+#'
+#' @param object Object of class \linkS4class{StfwfSchema}.
+#'
+#' @return A character vector with component entries \code{'num'}, \code{'char'}, and/or
+#' \code{'bool'}).
+#'
+#' @examples
+#' # A trivial example:
+#' df <- data.frame(variable = c('Turnover', 'Employees'),
+#'                  length = c(9L, 3L),
+#'                  initialPos = c(1, 10),
+#'                  finalPos = c(9, 12),
+#'                  type = rep('num', 2),
+#'                  valueRegEx = c('[0-9]{0,9}', '[0-9]{0,3}'),
+#'                  description = c('Turnover of the business unit',
+#'                                  'Number of employees of the business unit'),
+#'                  stringsAsFactors = FALSE)
+#' Schema <- new(Class = 'StfwfSchema', df = df)
+#' getTypes(Schema)
+#'
+#'
+#' @export
+setGeneric("getTypes", function(object){standardGeneric("getTypes")})
+
+#' @rdname getTypes
+#'
+#' @export
+setMethod(
+  f = "getTypes",
+  signature = c("StfwfSchema"),
+  function(object){object@df[['type']]}
+)
+
