b43f9836a6ea518f52070674e32c5858f1121e4f
david-salgado <david.salgado.fernandez@gmail.com>
2018-12-08 00:17:44 +0100

    Update doc for validateValues

--- R/validateValues.R
+++ R/validateValues.R
@@ -7,22 +7,14 @@
 #' This method is indeed a wrapper for the function \code{\link[data.table]{fread}} followed by the
 #' application of \code{\link[stringi]{stri_sub}}.
 #'
-#' @param filename Character vector of length 1 with the name of the file to read.
+#' @param object Object with rectangular shape whose columns will be validated.
 #'
-#' @param StfwfSchema Object of class \linkS4class{StfwfSchema} with the schema of the file to read.
-#'
-#' @param encoding Character vector of length 1 with default value is "unknown". Other possible
-#' options are "UTF-8" and "Latin-1".
-#' NB: it is not used to re-encode the input but to enable handling of encoded strings in their
-#' native encoding.
-#'
-#' @param check Logical vector of length 1 with default value \code{FALSE} to check if the values of
-#' each variable in the read file satisfy the regexp specified in the schema.
+#' @param StfwfSchema Object of class \linkS4class{StfwfSchema} with the schema of object.
 #'
 #' @param perl Logical vector of length 1 with default value \code{FALSE} to indicate whether to use
 #' perl or not in the application of regexp.
 #'
-#' @return Returns a \linkS4class{data.table} with the contents of the file.
+#' @return Returns \code{TRUE}.
 #'
 #' @examples
 #' \dontrun{
@@ -31,18 +23,13 @@
 #' stSchema <- fastReadfwf::XLSToSchema(pathSchema, sheetname = 'stEPA2018_Schema', lang = 'en')
 #' dataFile_T1 <- file.path(path, 'EPA/md_EPA_2018T1.txt')
 #' data_T1_st <- fastReadfwf::fread_fwf(dataFile_T1, stSchema, perl = TRUE)
-#' widths <- getLengths(stSchema)
-#' names <- getVariables(stSchema)
-#' data_T1 <- readr::read_fwf(dataFile_T1, readr::fwf_widths(widths, names))
+#' validateValues(data_T1_st, stSchema, perl = TRUE)
 #'
 #' }
-#' @seealso \code{\link[data.table]{fread}}
 #'
 #' @import data.table
 #'
-#' @importFrom stringi stri_sub
-#'
-#' @include StfwfSchema-class.R getdf.R getVariables.R getRegEx.R
+#' @include StfwfSchema-class.R getVariables.R getRegEx.R
 #'
 #' @export
 setGeneric("validateValues",
